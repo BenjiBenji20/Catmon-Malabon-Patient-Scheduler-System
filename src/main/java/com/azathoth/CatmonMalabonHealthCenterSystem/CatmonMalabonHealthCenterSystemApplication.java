9924dd1f725a979015217bde18ad053626df9939
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-03-05 14:23:16 +0800

    First commit. Started spring project to rewrite Catmon Malabon Health CEnter

---
+++ src/main/java/com/azathoth/CatmonMalabonHealthCenterSystem/CatmonMalabonHealthCenterSystemApplication.java
@@ -0,0 +1,13 @@
+package com.azathoth.CatmonMalabonHealthCenterSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmonMalabonHealthCenterSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmonMalabonHealthCenterSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azathoth/CatmonMalabonHealthCenterSystem/CatmonMalabonHealthCenterSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azathoth.CatmonMalabonHealthCenterSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmonMalabonHealthCenterSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
