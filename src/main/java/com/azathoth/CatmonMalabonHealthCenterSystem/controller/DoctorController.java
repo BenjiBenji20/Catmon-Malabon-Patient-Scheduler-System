d58e44cad4f33725f045b1395de3f709460ece31
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-03-06 11:23:55 +0800

    Created an api to get all the patients of doctor by its id

--- src/main/java/com/azathoth/CatmonMalabonHealthCenterSystem/controller/DoctorController.java
+++ src/main/java/com/azathoth/CatmonMalabonHealthCenterSystem/controller/DoctorController.java
@@ -1,17 +1,18 @@
 package com.azathoth.CatmonMalabonHealthCenterSystem.controller;
 
 import com.azathoth.CatmonMalabonHealthCenterSystem.dto.DoctorAuthenticationDTO;
+import com.azathoth.CatmonMalabonHealthCenterSystem.dto.PatientDTO;
+import com.azathoth.CatmonMalabonHealthCenterSystem.exception.ResourceNotFoundException;
 import com.azathoth.CatmonMalabonHealthCenterSystem.service.DoctorService;
 import jakarta.validation.Valid;
 import org.hibernate.exception.DataException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -46,6 +47,29 @@ public class DoctorController {
             return ResponseEntity.internalServerError().body(Map.of("error", "Error database"));
         }
         catch (Exception e) {
+            return ResponseEntity.internalServerError().body(Map.of("error", "Server error"));
+        }
+    }
+
+    /**
+     * Get all my patients
+     * Signed in doctor should pass his id here
+     */
+    @GetMapping("/private/get-all-my-patients/{myId}")
+    public ResponseEntity<?> getAllMyPatients(@PathVariable Long myId) {
+        try {
+            List<PatientDTO> allMyPatients = doctorService.getAllMyPatients(myId);
+
+            return allMyPatients.isEmpty() ?
+                    ResponseEntity.noContent().build() :
+                    ResponseEntity.ok().body(allMyPatients);
+        }
+        catch (ResourceNotFoundException e) {
+            logger.error("Doctor not found by id: {}", myId, e);
+            return ResponseEntity.notFound().build();
+        }
+        catch (Exception e) {
+            logger.error("Error fetching patients by id: {}", myId, e);
             return ResponseEntity.internalServerError().body(Map.of("error", "Server error"));
         }
     }
--- src/main/java/com/azathoth/CatmonMalabonHealthCenterSystem/service/DoctorService.java
+++ src/main/java/com/azathoth/CatmonMalabonHealthCenterSystem/service/DoctorService.java
@@ -1,25 +1,37 @@
 package com.azathoth.CatmonMalabonHealthCenterSystem.service;
 
 import com.azathoth.CatmonMalabonHealthCenterSystem.dto.DoctorAuthenticationDTO;
+import com.azathoth.CatmonMalabonHealthCenterSystem.dto.PatientDTO;
+import com.azathoth.CatmonMalabonHealthCenterSystem.model.Appointment;
 import com.azathoth.CatmonMalabonHealthCenterSystem.model.Doctor;
+import com.azathoth.CatmonMalabonHealthCenterSystem.model.Patient;
+import com.azathoth.CatmonMalabonHealthCenterSystem.repository.AppointmentRepository;
 import com.azathoth.CatmonMalabonHealthCenterSystem.repository.DoctorRepository;
+import com.azathoth.CatmonMalabonHealthCenterSystem.repository.PatientRepository;
 import jakarta.validation.Valid;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class DoctorService {
     private final DoctorRepository doctorRepository;
+    private final AppointmentRepository appointmentRepository;
+    private final PatientRepository patientRepository;
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
 
-    public DoctorService(DoctorRepository doctorRepository, AuthenticationManager authenticationManager, JwtService jwtService) {
+    public DoctorService(DoctorRepository doctorRepository, AppointmentRepository appointmentRepository, PatientRepository patientRepository, AuthenticationManager authenticationManager, JwtService jwtService) {
         this.doctorRepository = doctorRepository;
+        this.appointmentRepository = appointmentRepository;
+        this.patientRepository = patientRepository;
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
     }
@@ -40,5 +52,45 @@ public class DoctorService {
 
         return Optional.empty();
 
+    }
+
+    public List<PatientDTO> getAllMyPatients(Long myId) {
+        // get all the appointments for the doctor
+        List<Appointment> appointments  = appointmentRepository.findByDoctorId(myId);
+
+        // get all the patient id associated with doctor id
+        List<Long> patientIds = appointments
+                .stream().map(appointment ->
+                        appointment.getPatient().getId())
+                .toList();
+
+        if(patientIds.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        // fetch patient by collected id
+        return convertAllPatientsDTO(patientIds);
+    }
+
+    private List<PatientDTO> convertAllPatientsDTO(List<Long> patientIds) {
+        List<PatientDTO> patientDTOS = new ArrayList<>();
+
+        patientIds.forEach(id -> {
+            PatientDTO dto = new PatientDTO();
+            Patient patient = patientRepository.findPatientById(id);
+            dto.setId(patient.getId());
+            dto.setCompleteName(patient.getCompleteName());
+            dto.setGender(patient.getGender());
+            dto.setAddress(patient.getAddress());
+            dto.setAge(patient.getAge());
+            dto.setContactNumber(patient.getContactNumber());
+            dto.setVerificationNumber(patient.getVerificationNumber());
+            dto.setScheduleDate(patient.getAppointment().getScheduleDate());
+            dto.setStatus(patient.getAppointment().getStatus());
+
+            patientDTOS.add(dto);
+        });
+
+        return patientDTOS;
     }
 }
